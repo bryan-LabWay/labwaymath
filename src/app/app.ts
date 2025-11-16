05bfb20885f2424f980c98a86518bd43db888b8f
bryanLABWay <triomedlis@gmail.com>
2025-11-16 19:53:20 +0800

    Contact Us feaeture

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Footer } from "./section/footer/footer";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

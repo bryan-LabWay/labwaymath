21c02d4eef50b2535fbae30ec5d23f17cfc95ac9
triomedLIS <triomedlis@gmail.com>
2025-11-16 11:02:38 +0800

    Testimonial

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router'
 import { Hero } from '../../section/hero/hero';
-import { Testimonial } from '../../testimonial/testimonial';
+import { Testimonial } from '../../section/testimonial/testimonial';
+import { HowItWorks } from '../../section/how-it-works/how-it-works';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterLink, Testimonial, Hero],
+  imports: [RouterLink, Testimonial, Hero, HowItWorks],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
-export class Landing {
+export class Landing {  
+  constructor(private viewportScroller: ViewportScroller) {}
 
+  scrollToHowItWorks(): void {
+    this.viewportScroller.scrollToAnchor('how-it-works');
+  }
 }
